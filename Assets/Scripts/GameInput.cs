41932c34f2fb93aba95fd466022366509b30f861
D-Ulriken <dimonkv2000@gmail.com>
2023-03-03 18:51:13 +0200

    InputSystem
    Reconfiguration animations and add new InpytSystem

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputAction;
+    private void Awake()
+    {
+        playerInputAction = new PlayerInputActions();
+        playerInputAction.Player.Enable();
+    }
+    public Vector2 GetMovmentVectorNormalized()
+     {
+        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+     }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,24 +6,15 @@ using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private GameInput gameInput;
+
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private float turnSpeed = 10f;
     private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-            inputVector.y = +1;
-        if (Input.GetKey(KeyCode.S))
-            inputVector.y = -1;
-        if (Input.GetKey(KeyCode.A))
-            inputVector.x = -1;
-        if (Input.GetKey(KeyCode.D))
-            inputVector.x = +1;
-
-        inputVector = inputVector.normalized;
+        Vector3 inputVector = gameInput.GetMovmentVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
